3764d56bc6cd68b9228769119719124cbafdc53e
Bret Lowery <bret.lowery@gmail.com>
2020-02-03 14:24:11 -0500

    Support Spark Structured Streaming

--- spextral/analyze/spark.py
+++ spextral/analyze/spark.py
@@ -1,4 +1,3 @@
-import ast
 import os
 import stat
 import string
@@ -9,8 +8,8 @@ findspark.init()
 
 from pyspark import SparkContext, SparkConf
 from py4j.protocol import Py4JJavaError
-from pyspark.sql import Row, DataFrame, SQLContext, SparkSession
-from pyspark.sql.types import TimestampType, StringType, StructType, StructField, ArrayType, IntegerType
+from pyspark.sql import SparkSession, Row, DataFrame, SQLContext
+from pyspark.sql.types import StringType, StructType, ArrayType
 from pyspark.sql.functions import *
 
 from spextral import globals
@@ -56,36 +55,63 @@ class Spark(SpextralAnalyzer):
 
     @property
     def message_schema(self):
-        spextralhost = StructType([
-            StructField("name", StringType(), True),
-            StructField("fqdn", StringType(), True),
-            StructField("ips", StringType(), True),
-        ])
-        spextralmeta = StructType([
-            StructField("sent", TimestampType(), True),
-            StructField("spxv", StringType(), True),
-            StructField("host", spextralhost, True),
-        ])
-        spextraldata = StructType([
-            StructField("spxtrlbkt", StringType(), True),
-            StructField("spxtrldata", StringType(), True),
-            StructField("spxtrlevt1", TimestampType(), True),
-            StructField("spxtrlevt2", IntegerType(), True),
-            StructField("spxtrlevt3", IntegerType(), True),
-            StructField("spxtrlhost", StringType(), True),
-            StructField("spxtrlid", StringType(), True),
-            StructField("spxtrlsrc", StringType(), True),
-            StructField("spxtrltyp", StringType(), True),
-            StructField("spxtrlx", IntegerType(), True),
-        ])
-        spextralpacket = StructType(
-            StructField("meta", spextralmeta, True),
-            StructField("data", spextraldata, True),
-        )
-        spextralmessage = StructType(
-            StructField("spxtrl", spextralpacket, True),
-        )
-        return spextralmessage
+        """
+        Example message JSON:
+
+            {
+              "spxtrl": {
+                "meta": {
+                  "sent": "2020-02-01T17:05:44.099229",
+                  "spxv": "0.0.1",
+                  "spxh": {
+                    "name": "MacBook-Pro.lan",
+                    "fqdn": "macbook-pro.lan",
+                    "ips": "192.168.9.161"
+                  }
+                },
+                "data": [
+                    {
+                        "spxtrlbkt": "flan",
+                        "spxtrldata": "91.224.160.4 - - [15/Oct/2019:17:54:08 -0400] \"POST /wp-login.php HTTP/1.0\" 200 2967 \"http://nationalphilosophicalcounselingassociation.org/\" \"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.162 Safari/535.19 Flan/0.0.37 (https://bret.guru/flan)\"",
+                        "spxtrlephn": "MacBook-Pro.lan",
+                        "spxtrlid": "e52d1ce64560514646aa",
+                        "spxtrlsrc": "/var/bret/out/access.log",
+                        "spxtrlstyp": "bret_access_combined",
+                        "spxtrlts1": "15/Oct/2019:17:54:08 -0400",
+                        "spxtrlts2": "20191015175408",
+                        "spxtrlts3": "1571176448",
+                        "spxtrlx": "1571198413"
+                    } (,...)
+                ]
+              }
+            }
+
+        """
+        spxh = StructType()\
+            .add("name", StringType(), nullable=False)\
+            .add("fqdn", StringType(), nullable=False)\
+            .add("ips", StringType(), nullable=False)
+        meta = StructType()\
+            .add("sent", StringType(), nullable=False)\
+            .add("spxv", StringType(), nullable=False)\
+            .add("spxh", spxh)
+        event = StructType()\
+            .add("spxtrlbkt", StringType(), nullable=False)\
+            .add("spxtrldata", StringType(), nullable=False)\
+            .add("spxtrlephn", StringType(), nullable=False)\
+            .add("spxtrlid", StringType(), nullable=False)\
+            .add("spxtrlsrc", StringType(), nullable=False)\
+            .add("spxtrlstyp", StringType(), nullable=False)\
+            .add("spxtrlts1", StringType(), nullable=False)\
+            .add("spxtrlts2", StringType(), nullable=False)\
+            .add("spxtrlts3", StringType(), nullable=False)\
+            .add("spxtrlx", StringType(), nullable=False)
+        spxtrl = StructType().\
+            add("meta", meta).\
+            add("data", ArrayType(event))
+        schema = StructType().\
+            add("spxtrl", spxtrl)
+        return schema
 
     @timeout_after(60)
     def _setcontext(self):
@@ -163,18 +189,15 @@ class Spark(SpextralAnalyzer):
         except Exception as e:
             self.error("reading from %s transport stream at %s: %s" % (self.integration.capitalize(), self.target, str(e)))
         results = stream \
-            .selectExpr("CAST(value AS STRING) as spx") \
-            .select(from_json("spx", self.message_schema))
-        results.printSchema()
-        # meta = results.select(
-        # meta.printSchema()
-        # meta.start()
-        # meta.awaitTerminationOrTimeout(90)
-        # meta.stop(stopGraceFully=True)
+            .selectExpr("CAST(value AS STRING) as spxmsgraw") \
+            .select(from_json("spxmsgraw", self.message_schema)) \
+            .alias("spxmsg") \
+            .select("spxmsg.*") \
+            .writeStream \
+            .format("console") \
+            .start() \
+            .awaitTermination()
 
-
-         ##   .select("siteId", "siteData.dataseries", explode("siteData.values").alias("values")) \
-         ##   .select("siteId", "dataseries", "values.*")
 
     def close(self, **kwargs):
         pass
